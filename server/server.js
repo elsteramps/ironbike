9ca586cf057b4432dd0f68af81a6b9519c398d71
unknown <elstermetalhead@gmail.com>
2023-11-15 11:57:21 +0100

    process env edited

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require("nodemailer");
+require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // Ustaw obsługę CORS
 app.use(cors());
